ff118b6fc6944aac0cc4758498583c85d6cfff49
Jeffrey Hermo <Jhermo2967@conestogac.on.ca>
2022-02-04 09:11:07 -0500

    Containing the completed conversion class

---
+++ Assignment02/Conversion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public class Conversion
+    {
+
+        private int conversionObj;
+
+        public Conversion()
+        {
+            conversionObj = 1;
+        }
+
+        public Conversion(int conversionObj)
+        {
+            this.conversionObj = conversionObj;
+        }
+
+        public double ConvertCelciusToFahrenheit()
+        {
+            double value = (conversionObj * 1.8) + 32;
+            return value;
+        }
+
+        public double ConvertCelciusToKelvin()
+        {
+            double value = (conversionObj + 273) ;
+            return value;
+        }
+
+        public double ConvertKelvinToFahrenheit()
+        {
+            double value = (1.8 * (conversionObj - 273)) + 32;
+            return value;
+        }
+
+        public double ConvertKelvinToCelsius()
+        {
+            double value = conversionObj - 273;
+            return value;
+        }
+
+        public double ConvertFahrenheitToKelvin()
+        {
+            double value = (((conversionObj-32) * 5) / 9) + 273.15;
+            return value;
+        }
+
+        public double ConvertFahrenheitToCelsius()
+        {
+            double value = (conversionObj - 32) * 1.8;
+            return value;
+        }
+
+    }
+}
